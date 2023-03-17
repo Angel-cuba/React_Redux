4b47e419bf532d2452d5cd7e9dceee723e07e1fa
Mac_Developer <araozangel842@gmail.com>
2023-03-17 23:23:22 +0200

    Add component to render data

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { GetUsers } from '../redux/actions/UserActions';
 
-const Users = () => {
-  
+function Users() {
+
+const { users } =useSelector(state => state);
+  console.log("🚀 ~ file: Users.js:8 ~ Users ~ users:" , users)
+  const dispatch = useDispatch();
+
+   useEffect(() => {
+     dispatch(GetUsers());
+   }, [dispatch])
+
+
+const handleClick = () => {
+ dispatch(GetUsers()); 
+}
   return (
-    <div>Users</div>
+    <div>Users
+
+      <button onClick={handleClick}>click</button>
+      {/* {
+        users?.map(user => (
+          <div key={user.id}>
+            <p>{user.name}</p>
+          </div>
+        ))
+      } */}
+    </div>
   )
 }
 
