7707aff618f38872ec01969d2f803a14f4289ca1
Mac_Developer <araozangel842@gmail.com>
2023-03-17 23:21:29 +0200

    Keep all request in same file

--- src/api/request.js
+++ src/api/request.js
@@ -1,17 +1,17 @@
 export const allUsers = async () => {
-  await fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(json => console.log(json));
-}
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await response.json();
+  return data;
+};
 
 export const commentsByPostId = async (id) => {
-  await fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
-    .then(response => response.json())
-    .then(json => console.log(json));
-}
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`);
+  const comments = await response.json();
+  return comments;
+};
 
 export const postsByUserId = async (postId) => {
-    await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`)
-      .then(response => response.json())
-      .then(json => console.log(json));
-}
+  const response = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`);
+  const posts = await response.json();
+  return posts;
+};
