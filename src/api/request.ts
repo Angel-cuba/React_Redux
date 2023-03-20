7f233653db9d12c28a0acf8fee9486bf0ee27917
Mac_Developer <araozangel842@gmail.com>
2023-03-20 22:19:38 +0200

    Change files extension

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,16 +1,16 @@
+import axios from 'axios';
 export const allUsers = async () => {
-  const response = await fetch('https://jsonplaceholder.typicode.com/users');
-  const data = await response.json();
-  return data;
+  const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+  return response.data;
 };
 
-export const commentsByPostId = async (id) => {
+export const commentsByPostId = async (id: string) => {
   const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`);
   const comments = await response.json();
   return comments;
 };
 
-export const postsByUserId = async (postId) => {
+export const postsByUserId = async (postId: string) => {
   const response = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`);
   const posts = await response.json();
   return posts;
